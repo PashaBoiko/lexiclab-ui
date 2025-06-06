17985204ae4561b18505a27c4413c1104e01d614
pavel.boiko <pboiko@corcentric.com>
2025-06-06 17:50:16 +0200

    fix: fixed prettier

--- src/services/auth/auth.test.ts
+++ src/services/auth/auth.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('sum function', () => {
-  it('correctly adds two numbers', () => {
+describe("sum function", () => {
+  it("correctly adds two numbers", () => {
     expect(true).toBe(true);
   });
 });
