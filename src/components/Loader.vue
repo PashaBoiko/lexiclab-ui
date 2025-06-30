c6f9f9b82ca8c3fd0c95aeb84753aae2e997400a
pavel.boiko <pboiko@corcentric.com>
2025-06-30 19:52:27 +0300

    fix: small fix

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,4 +1,5 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <div class="ll-loader">
@@ -15,8 +16,8 @@
   position: absolute;
   right: 0;
   left: 0;
-  top: 0;
   bottom: 0;
+  top: 0;
   display: flex;
   justify-content: center;
   align-items: center;
